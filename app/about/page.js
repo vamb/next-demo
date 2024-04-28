07784b1abc1f376da4f789275473bb51727097d0
zhangsy <zhangsy@think-land.com>
2024-04-28 16:58:29 +0800

    feat: code update

--- app/about/page.js
+++ app/about/page.js
@@ -18,12 +18,28 @@ export default () => {
   const pathname = usePathname()
   console.log('about pathname', pathname) // '/about'
 
+  const handleBtnClick = (sortOrder) => {
+    const params = new URLSearchParams(searchParams.toString())
+    params.set('sort', sortOrder)
+    window.history.pushState(null, '', `?${params.toString()}`)
+  }
+
   return (
     <div
-      className={'lex flex-col items-center justify-between p-2 bg-orange-500 flex-grow'}
+      className={'flex flex-col items-start justify-start p-2 bg-orange-500 flex-grow gap-1'}
     >
       <div>{`About us search param: ${result? JSON.stringify(result): ''}`}</div>
       <div>{`About us pathname: ${pathname}`}</div>
+      <div className={'flex flex-row gap-2'}>
+        <div
+          className={'self-btn'}
+          onClick={()=>handleBtnClick('asc')}
+        >sort asc</div>
+        <div
+          className={'self-btn'}
+          onClick={()=>handleBtnClick('desc')}
+        >sort desc</div>
+      </div>
     </div>
   )
 }
